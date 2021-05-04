057c48a45ea8b7d2f2024cf2b38132309cda1b7b
Kalyana Kondapalli <kalyana.kondapalli@ttec.com>
2021-05-04 14:36:52 +0530

    getEventsWithWorkshops task completed

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/getEventsWithWorkshops', [EventsController::class, 'getEventsWithWorkshops']);
+Route::get('/events', [EventsController::class, 'getEventsWithWorkshops']);
