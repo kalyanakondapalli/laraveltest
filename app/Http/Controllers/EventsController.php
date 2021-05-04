7f41953b04448af4005295c16d5ef544d23ca752
Kalyana Kondapalli <kalyana.kondapalli@ttec.com>
2021-05-04 14:34:47 +0530

    getEventsWithWorkshops task completed

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -97,7 +97,8 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        $events = Event::with('workshop')->get();
+        return Response::json($events, 200);
     }
 
 
